338425a7ce6b49adaed13d4dc99373400458cdf1
rafa mass <rafamass@rm>
2026-04-25 12:33:43 -0300

    feat: adiciona modo leitor claro e escuro

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   base: '/vereda/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reader: resolve(__dirname, 'reader.html'),
+        readerDark: resolve(__dirname, 'reader-dark.html'),
+      },
+    },
+  },
 })
